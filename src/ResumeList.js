e09dbc4daa22a153ac4a2b23460f31fc01b848cd
ZahakHussain <zahakhussan@gmail.com>
2025-07-16 21:25:54 +0500

    updating ResumeList.js view list button fetchresume

--- src/ResumeList.js
+++ src/ResumeList.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import api from "./api";
 import { useNavigate } from "react-router-dom";
 // import axios from "axios";
@@ -9,7 +9,7 @@ const ResumeList = () => {
 
 
 
-  const fetchResumes = async () => {
+  const fetchResumes = useCallback(async () => {
     try {
       const token = localStorage.getItem("token");
       const res = await api.get("/api/resumes", {
@@ -21,7 +21,8 @@ const ResumeList = () => {
       alert("Session expired. Please login again.");
       navigate("/login");
     }
-  };
+  },
+    [navigate]);
 
   const handleDelete = async (id) => {
     try {
@@ -43,38 +44,40 @@ const ResumeList = () => {
     if (!token) return navigate("/login");
 
     fetchResumes();
-  }, [navigate]);
+  }, [navigate, fetchResumes]);
+
+};
 
 
-  return (
-    <div className="p-4">
+return (
+  <div className="p-4">
 
-      <div className="flex justify-between items-center">
-        <h1 className="text-2xl font-bold mb-4">Your Resumes</h1>
-        <button
-          onClick={() => navigate("/resume")}
-          className="mt-4 bg-blue-500 text-white px-4 py-2 rounded"
-        >Back to Builder
-        </button>
-      </div>
-
-      <ul className="space-y-2">
-        {resumes.map((resume) => (
-          <li key={resume._id} className="border p-3 rounded shadow-sm">
-            <h2 className="font-semibold">{resume.name}</h2>
-            <p>Email: {resume.email}</p>
-            <p>Phone: {resume.phone}</p>
-            <p>Skills: {resume.skills.join(", ")}</p>
-            <button
-              onClick={() => handleDelete(resume._id)}
-              className="mt-2 bg-red-500 text-white px-3 py-1 rounded"
-            >Delete</button>
-          </li>
-        ))}
-
-      </ul>
+    <div className="flex justify-between items-center">
+      <h1 className="text-2xl font-bold mb-4">Your Resumes</h1>
+      <button
+        onClick={() => navigate("/resume")}
+        className="mt-4 bg-blue-500 text-white px-4 py-2 rounded"
+      >Back to Builder
+      </button>
     </div>
-  );
+
+    <ul className="space-y-2">
+      {resumes.map((resume) => (
+        <li key={resume._id} className="border p-3 rounded shadow-sm">
+          <h2 className="font-semibold">{resume.name}</h2>
+          <p>Email: {resume.email}</p>
+          <p>Phone: {resume.phone}</p>
+          <p>Skills: {resume.skills.join(", ")}</p>
+          <button
+            onClick={() => handleDelete(resume._id)}
+            className="mt-2 bg-red-500 text-white px-3 py-1 rounded"
+          >Delete</button>
+        </li>
+      ))}
+
+    </ul>
+  </div>
+);
 }
 
 export default ResumeList;
